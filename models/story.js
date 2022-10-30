02b882d10bf5df2d0db90b28b6d6e3aff84316ec
David Acosta <davidacosta.dev@gmail.com>
2022-10-29 23:49:45 -0700

    for good measure

--- models/story.js
+++ models/story.js
@@ -22,5 +22,11 @@ storySchema.pre('find', function (next) {
 const Story = mongoose.model('Story', storySchema, 'stories')
 
 module.exports = { Story }
+
  
 
+//NOTES:
+    //author has a type of Schema that refs 'User' model.
+    //this means that you can link them.
+    //BUT if you want to populate the author's info, you'll need to use Mongoose hooks like .pre which allows you to 
+    //pre-query and populate the author's info that is referenced in the author key.
--- server.js
+++ server.js
@@ -2,17 +2,25 @@ const express = require('express')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const cookieParser = require('cookie-parser')
+var cors = require('cors');
 
-//authentication
-const { check } = require('express-validator')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+//---------------
+const imgbbUploader = require("imgbb-uploader");
+/* or use import in ESM projects:
+import { imgbbUploader } from "imgbb-uploader"; 
+*/
+//----------
+
+// //authentication
+// const bcrypt = require('bcryptjs')
+// const jwt = require('jsonwebtoken')
 
 //Instead of using mongoose's promise-like system, we'll be using Javascript's promise system: 
 mongoose.Promise = global.Promise
 
 
 const { DATABASE_URL, PORT, JWT_KEY_SECRET } = require('./config')
+//inert Routers here *******
 const storiesRouter = require('./routers/storiesRouter')
 const usersRouter = require('./routers/usersRouter')
 
@@ -23,6 +31,29 @@ app.use(express.json()) //looks at any requests that comes in that has json in i
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 app.use(cookieParser())
+// app.use(cors())
+
+//rolled out our own cors middleware
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Origin, X-Requested-With, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  next();
+});
+
+app.post('/monkey', cors(), (req, res, next) => {
+    const options ={
+        apiKey: 'cacaa3c91ff066da98b3a1e60e1fe8d0', // MANDATORY apikey for imgBB
+        base64string: req.body.base64string,
+        // OPTIONAL: pass base64-encoded image (max 32Mb)
+      }
+    imgbbUploader(options)
+  .then((response) => {
+    res.json(response);
+})
+  .catch((error) => console.error(error));
+})
 
 
 //STORY ROUTES USE TO BE HERE 
@@ -40,6 +71,8 @@ mongoose.connect(DATABASE_URL, () => {
         console.log(`Your app is listening on port ${PORT}`)
     })
 })
+
+//now we have mongoose calling the app.listen to boot up the server, but ALSO he is connecting us to the DATABASE
 
 
 
